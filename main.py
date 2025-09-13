11152b6a67515a55a0ca94498ed44558e05f8866
Rishabh kumar patel <rishu050803@gmail.com>
2025-09-13 18:33:33 +0530

    models

--- main.py
+++ main.py
@@ -16,9 +16,15 @@ MODEL_DIR = os.path.join(os.path.dirname(__file__), 'model')
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://bhgreenpulse.netlify.app",
+        "http://localhost",
+        "http://localhost:3000",
+        "http://127.0.0.1",
+        "http://127.0.0.1:3000"
+    ],
     allow_credentials=True,
-    allow_methods=["*"], 
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
