45efd906f9e92a0faea14454f349624792c21f95
Rishabh kumar patel <82746979+Rishabh050803@users.noreply.github.com>
2025-09-18 21:58:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ app.add_middleware(
         "http://localhost",
         "http://localhost:3000",
         "http://127.0.0.1",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
